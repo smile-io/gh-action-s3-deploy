85ad8432a0dc01dfc63fa534df670d9f3f5ea9f1
Xavi Urtasun <xavi.urta@gmail.com>
2022-03-24 12:27:32 +0100

    removing action.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-$(aws sts assume-role \
---role-arn arn:aws:iam::307739032832:role/allow-auto-deploy-from-other-accounts \
---role-session-name AWSCLI-Session \
---query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
---output text))
